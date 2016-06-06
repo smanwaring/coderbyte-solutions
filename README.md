7f08baf2c51c10564dce04489df1b03d4be6bc1a
Stephanie Manwaring <stephanie.manwaring@gmail.com>
2016-06-05 21:24:15 -0400

    added additional solutions
    added additional solutions

--- README.md
+++
@@ -1,6 +0,0 @@
-# coderbyte-solutions
-coderbyte solutions (no use of regex)
-
----
-
-This is the place where I store my coderbyte solutions
--- first reverse/first-reverse.js
+++
@@ -1,7 +0,0 @@
-function FirstReverse(str) { 
-    var newStr = "";
-    for (var i = str.length-1; i > -1; i--) {
-        newStr += str[i]
-    }
-  return newStr;
-}
