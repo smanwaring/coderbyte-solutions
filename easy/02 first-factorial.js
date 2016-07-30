d68e3235f313d9292f0b01349cdf9656d1863478
Stephanie Manwaring <stephanie.manwaring@gmail.com>
2016-07-29 23:43:52 -0400

    added question

--- easy/02 first-factorial.js
+++ easy/02 first-factorial.js
@@ -1,3 +1,8 @@
+//Have the function LetterChanges(str) take the str parameter being passed and modify it using the following algorithm. 
+//Replace every letter in the string with the letter following it in the alphabet (ie. c becomes d, z becomes a). 
+//Then capitalize every vowel in this new string (a, e, i, o, u) and finally return this modified string. 
+
+
 function FirstFactorial(num) { 
     answer = 1;
     for (var i = num; i > 1; i--) {
