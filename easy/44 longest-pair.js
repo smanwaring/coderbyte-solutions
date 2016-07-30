80555e7e7d7ec1e28d9c988be6dc56aba58fa671
Stephanie Manwaring <stephanie.manwaring@gmail.com>
2016-07-30 00:25:21 -0400

    added question

---
+++ easy/44 longest-pair.js
@@ -0,0 +1,3 @@
+//Using the JavaScript language, have the function LargestPair(num) take the num parameter being passed and determine the 
+//largest double digit number within the whole number. For example: if num is 4759472 then your program should return 94 because 
+//that is the largest double digit number. The input will always contain at least two positive digits. 
