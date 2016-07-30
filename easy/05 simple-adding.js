114a30d2674146b17d6fab5c10a324b650528f80
Stephanie Manwaring <stephanie.manwaring@gmail.com>
2016-07-29 23:48:33 -0400

    added question

--- easy/05 simple-adding.js
+++ easy/05 simple-adding.js
@@ -1,3 +1,6 @@
+//Have the function SimpleAdding(num) add up all the numbers from 1 to num. 
+//For the test cases, the parameter num will be any number from 1 to 1000. 
+
 function SimpleAdding(num) { 
     var answer = 0;
     for (var i = 1; i <= num; i++) {
