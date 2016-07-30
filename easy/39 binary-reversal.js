884e380e1e5c30969f47856ff1332c4663ec8cd7
Stephanie Manwaring <stephanie.manwaring@gmail.com>
2016-07-30 00:21:18 -0400

    added question

---
+++ easy/39 binary-reversal.js
@@ -0,0 +1,5 @@
+//Using the JavaScript language, have the function BinaryReversal(str) take the str parameter being passed, 
+//which will be a positive integer, take its binary representation, reverse that string of bits, and then finally 
+//return the new reversed string in decimal form. For example: if str is "47" then the binary version of this integer is 
+//00101111. Your program should reverse this binary string which then becomes: 11110100 and then finally return the decimal 
+//version of this string, which is 244. 
