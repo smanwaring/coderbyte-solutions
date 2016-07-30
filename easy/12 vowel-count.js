b48249114233e0e594d415cd80812466f889a200
Stephanie Manwaring <stephanie.manwaring@gmail.com>
2016-07-29 23:54:12 -0400

    added question

--- easy/12 vowel-count.js
+++ easy/12 vowel-count.js
@@ -1,3 +1,7 @@
+//Have the function VowelCount(str) take the str string parameter being passed and return the number of vowels the string contains 
+//(ie. "All cows eat grass" would return 5). 
+//Do not count y as a vowel for this challenge.
+
 function VowelCount(str) { 
     var vowels = "aeiou";
     str = str.toLowerCase();
