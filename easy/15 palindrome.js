be5721579eef66a96c3e552505fb1af95a29d1d0
Stephanie Manwaring <stephanie.manwaring@gmail.com>
2016-07-29 23:56:34 -0400

    added question

--- easy/15 palindrome.js
+++ easy/15 palindrome.js
@@ -1,3 +1,7 @@
+//Have the function Palindrome(str) take the str parameter being passed and return the string true if the parameter is a 
+//palindrome, (the string is the same forward as it is backward) otherwise return the string false. For example: 
+//"racecar" is also "racecar" backwards. Punctuation and numbers will not be part of the string. 
+
 function Palindrome(str) { 
     str = str.toLowerCase();
     var newStr = "";
