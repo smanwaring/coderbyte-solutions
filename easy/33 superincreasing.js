5d70f3c993bfccae00a9af3a62d2ecf76e075a27
Stephanie Manwaring <stephanie.manwaring@gmail.com>
2016-07-30 00:12:39 -0400

    added question

--- easy/33 superincreasing.js
+++ easy/33 superincreasing.js
@@ -1,3 +1,9 @@
+//Have the function Superincreasing(arr) take the array of numbers stored in arr and determine if the array forms a 
+//superincreasing sequence where each element in the array is greater than the sum of all previous elements. 
+//The array will only consist of positive integers. For example: if arr is [1, 3, 6, 13, 54] then your program should return the 
+//string "true" because it forms a superincreasing sequence. 
+//If a superincreasing sequence isn't formed, then your program should return the string "false" 
+
 function Superincreasing(arr) { 
     var counter = arr[0];
     
