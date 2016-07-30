155b6787fb158b98e7c52d64f833be1bdb9a5613
Stephanie Manwaring <stephanie.manwaring@gmail.com>
2016-07-29 23:49:11 -0400

    added question

--- easy/06 letter-capitalize.js
+++ easy/06 letter-capitalize.js
@@ -1,3 +1,6 @@
+//Have the function LetterCapitalize(str) take the str parameter being passed and capitalize the first letter of each word. 
+//Words will be separated by only one space. 
+
 function LetterCapitalize(str) { 
 	var answer = "";
 	for (var i = 0; i < str.length; i++) {
